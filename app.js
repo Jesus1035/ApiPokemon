55ba5df5a7fdaddc776da96b2f3e2b14b1adb1c9
jesus1035 <jesus480b@gmail.com>
2026-04-21 19:53:38 -0600

    Arreglas HTML para que se vean estilos

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const API_URL="https://pokeapi.co/api/v2/pokemon/${param}";
-const MAX_Equipo=6;
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+const MAX_EQUIPO = 6;
 
 let pokemonActual = null;
 let equipo = [];
+
 
 const inputNombre = document.getElementById('inputNombre');
 const idBuscarNombre = document.getElementById('idBuscarNombre');
 const btn_aleatorio = document.getElementById('btn_aleatorio');
 const ContenedorFicha = document.getElementById('ContenedorFicha');
 const btnAgregar = document.getElementById('btnAgregar');
-const tablaEquipo = document.querySelector('#tablaEquipo tbody');
+const tablaEquipo = document.getElementById('tablaEquipo');
 const alerta = document.getElementById('alerta');
 const contadorEquipo = document.getElementById('contadorEquipo');
 
+
 idBuscarNombre.addEventListener('click', () => {
-    const nombre = inputNombre.ariaValueMax.trim().toLowerCase();
+    const nombre = inputNombre.value.trim().toLowerCase();
     if (nombre) buscarPokemon(nombre);
 });
+
 btn_aleatorio.addEventListener('click', () => {
-    const idRandom = Math.floor(Math.random()*150) + 1;
+    const idRandom = Math.floor(Math.random() * 150) + 1;
     buscarPokemon(idRandom);
 });
 
